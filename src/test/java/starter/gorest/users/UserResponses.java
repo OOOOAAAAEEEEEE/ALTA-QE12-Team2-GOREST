862e23f7a4f31b20dd68b778df4ec3c13d58e7e5
inaylalala <inaylalala@gmail.com>
2023-09-19 16:45:42 +0700

    init gorest user automation

---
+++ src/test/java/starter/gorest/users/UserResponses.java
@@ -0,0 +1,7 @@
+package starter.gorest.users;
+
+public class UserResponses {
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+}
+
---
+++ src/test/java/starter/utils/Constant.java
@@ -0,0 +1,10 @@
+package starter.utils;
+
+public class Constant {
+    public static final String BASE_URL = "https://gorest.co.in";
+    public static final String TOKEN = "27baa6f196efe3329d5a3fa5830d84ba7e8fade103a0182b64fea1717a0caa3f";
+    public static String DIR = System.getProperty("user.dir");
+    public static String JSON = DIR+"/src/test/resources/JSON";
+    public static String REQ_BODY = JSON+"/ReqBody/";
+    public static String JSON_SCHEMA = JSON+"/JSONSchema/";
+}
