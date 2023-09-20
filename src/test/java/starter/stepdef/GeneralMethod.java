69e742a993157e4b6a308736ea55cfc9117092db
Arif Laksonodhewo <arifldhewo234@gmail.com>
2023-09-20 16:48:10 +0700

    Adding Validate JSON Schema method, Updating BaseURL method

--- src/test/java/starter/stepdef/GeneralMethod.java
+++ src/test/java/starter/stepdef/GeneralMethod.java
@@ -2,8 +2,8 @@ package starter.stepdef;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import net.serenitybdd.rest.SerenityRest;
 import starter.utils.Constant;
 
@@ -11,12 +11,16 @@ import java.io.File;
 
 public class GeneralMethod {
 
-    public String endpoint;
+    public static String endpoint;
 
-
-    @Given("Base URL and path {string}")
+    @Given("Base URL and {string} as path")
     public void baseURLIsAndSetPath(String path) {
         endpoint = Constant.BASE_URL+path;
+    }
+
+    @Given("Base URL and {string} as path and {string} as pathParam and {string} as queryParam")
+    public void baseURLAndAsPathAndAsPathParamAndAsQueryParam(String path, String pathParam, String queryParam) {
+        endpoint = Constant.BASE_URL + path + pathParam + queryParam;
     }
 
     @Given("Passing {string} as json request into body and set contentType as Json")
@@ -29,13 +33,17 @@ public class GeneralMethod {
             .body(jsonFile);
     }
 
-    @When("Send request from given endpoint")
-    public void sendRequestFromGivenEndpoint() {
-        SerenityRest.when().post(endpoint);
-    }
-
     @Then("Status code should be {int}")
     public void statusCodeShouldBe(int code) {
         SerenityRest.then().statusCode(code);
     }
+
+
+
+//    @And("Validate JSON schema {string}")
+//    public void validateJSONSchema(String jsonPath) {
+//        File jsonFile = new File(Constant.JSON_SCHEMA+jsonPath);
+//        SerenityRest.then()
+//                .body(JsonSchemaValidator.matchesJsonSchema(jsonFile));
+//    }
 }
